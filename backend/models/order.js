029b54f2ee11f8352d3953cd42319293bbca2612
Mayank <mayanksahu1005@gmail.com>
2024-08-10 15:08:02 +0530

    driver and bid contoller added

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,87 +1,89 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
-    {
-        supplierId : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Supplier',
-            required: true,
+  {
+    supplierId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
+      required: true,
+    },
+    driverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Driver",
+      required: false,
+    },
+    pickUpLocation: {
+      type: String,
+      required: true,
+    },
+    deliveryLocation: {
+      type: String,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["waiting", "picked", "delivered"],
+      required: true,
+    },
+    pickUpTime: {
+      type: Date,
+      required: true,
+    },
+    deliveryTime: {
+      type: Date,
+      required: true,
+    },
+    orderAmount: {
+      type: Number,
+      required: true,
+    },
+    bids: [
+      {
+        driverId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Driver",
         },
-        driverId : {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Driver',
-            required: false,
+        bidAmount: {
+          type: Number,
+          required: true,
         },
-        pickUpLocation: {
-            type: String,
-            required: true,
+        bidTime: {
+          type: Date,
+          default: Date.now,
         },
-        deliveryLocation: {
-            type: String,
-            required: true,
+        status: {
+          type: String,
+          enum: ["pending", "accepted", "rejected"],
+          default: "pending",
         },
-        orderStatus: {
-            type: String,
-            enum: ["waiting", "picked", "delivered"],
-            required: true,
+      },
+    ],
+    driverLocation: {
+      type: {
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true },
+      },
+      required: false,
+    },
+    chat: [
+      {
+        senderId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
         },
-        pickUpTime: {
-            type: Date,
-            required: true,
+        message: {
+          type: String,
+          required: true,
         },
-        deliveryTime: {
-            type: Date,
-            required: true,
+        sentAt: {
+          type: Date,
+          default: Date.now,
         },
-        orderAmount: {
-            type: Number,
-            required: true,
-        },
-        bids: [
-            {
-              driverId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Driver',
-              },
-              bidAmount: {
-                type: Number,
-                required: true,
-              },
-              bidTime: {
-                type: Date,
-                default: Date.now,
-              },
-              status: {
-                type: String,
-                enum: ["pending", "accepted", "rejected"],
-                default: "pending",
-              },
-            }
-          ],
-          driverLocation: {
-            type: {
-              lat: { type: Number, required: true },
-              lng: { type: Number, required: true },
-            },
-            required: false,
-          },
-          chat: [
-            {
-              senderId: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User',
-              },
-              message: {
-                type: String,
-                required: true,
-              },
-              sentAt: {
-                type: Date,
-                default: Date.now,
-              },
-            },
-          ],
-        },{ timestamps: true }
-)
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const Order = mongoose.model("Order", orderSchema);
 
-export default mongoose.model('Order', orderSchema);
+export default Order;
