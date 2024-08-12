51eceab800798b6ba626bbe7dc921ca8249e4229
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-08-12 16:43:22 +0530

    Full Setup Backend ,frontend

--- client/src/components/Layout.jsx
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router"
-
-export default function Layout(){
-    return <div class="bg-gray-100 flex items-center justify-center min-h-screen">
-        <Outlet />
-    </div>
-}
