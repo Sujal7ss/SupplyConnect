49d6bdf9f2662ff73f3fb6ee9dd214d4470e1b8b
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2024-08-12 17:44:32 +0530

    ff

--- client/src/Context/MapContext.jsx
+++ client/src/Context/MapContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 
 export const MapContext = createContext({});
 
-const
+
 export const MapProvider  = ({children}) => {
     
     return <Authcontext.Provider value={{}}>
