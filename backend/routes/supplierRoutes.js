3c735ac3458c9024924731056557d53b0417e16e
Md Gulam Gaush <123100963+imgullu786@users.noreply.github.com>
2024-08-12 20:34:08 +0530

    auth updated

--- backend/routes/supplierRoutes.js
+++ backend/routes/supplierRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { createOrUpdateSupplierProfile, getSupplierProfile} from "../controller/supplierController.js";
+import { updateSupplierProfile, getSupplierProfile} from "../controller/supplierController.js";
 
 const router = express.Router();
 
 // Supplier
-router.post('/supplier/createORupdate', createOrUpdateSupplierProfile);
+router.post('/supplier/update', updateSupplierProfile);
 router.get('/suppliers', getSupplierProfile);
 
 export default router;
