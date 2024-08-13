c9e962b2aa619ede163e3feb34451b84f51ac3cf
Sujal7ss <sujalchahande3@gmail.com>
2024-08-13 16:54:48 +0530

    updated app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Orders from "./pages/Orders.jsx";
 import { AuthProvider } from "./Context/AuthContext.jsx";
 import OrderDetails from "./pages/OrderDetails.jsx";
 import BidsPage from "./pages/BidsPage.jsx";
+// import AllOrders from "./pages/AllOrders.jsx";
 
 function App() {
   return (
@@ -24,6 +25,9 @@ function App() {
               <Route path="orders" element={<Orders />} />
               <Route path="order-details/:id" element={<OrderDetails />} />
               <Route path="bids/:id" element={<BidsPage />}/>
+            </Route>
+            <Route path="/driver" element={<AppLayout />}>
+              <Route index element={<Orders />}/> 
             </Route>
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
