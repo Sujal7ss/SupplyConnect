4226cb6d0399aa606a2ea927f20d6f2ef7b0c207
Mayank <mayanksahu1005@gmail.com>
2024-08-16 02:31:30 +0530

    home bg image added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 import TruckLoading from "../components/TruckLoader";
 import { Link } from "react-router-dom";
+import Background from "../assets/bgimg.jpg";
 const Home = () => {
   return (
     <>
-      <Link to="/book-order" class="bg-blue-600 text-white py-4 px-8 rounded-full shadow-lg flex items-center space-x-3">
-        <svg
-          class="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+          height: "80vh",
+          justifyContent: "center",
+          backgroundImage: `url(${Background})`,
+          backgroundSize: "cover ",
+          backgroundPosition: "center",
+        }}
+      >
+        <Link
+          to="/book-order"
+          class="bg-blue-600 text-white py-4 px-8 rounded-full shadow-lg flex items-center space-x-3"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M11.049 2.927a1 1 0 00-1.098 0l-9 7a1 1 0 00-.13 1.426l6 8a1 1 0 001.514.083L12 12.414l4.665 5.021a1 1 0 001.486-.073l6-8a1 1 0 00-.113-1.426l-9-7z"
-          />
-        </svg>
-        <span class="text-lg">Select Pickup Point</span>
-      </Link>
+          <svg
+            class="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M11.049 2.927a1 1 0 00-1.098 0l-9 7a1 1 0 00-.13 1.426l6 8a1 1 0 001.514.083L12 12.414l4.665 5.021a1 1 0 001.486-.073l6-8a1 1 0 00-.113-1.426l-9-7z"
+            />
+          </svg>
+          <span class="text-lg">Select Pickup Point</span>
+        </Link>
+      </div>
     </>
   );
 };
