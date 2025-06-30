0c5d2b75f806793420ce81704287707aba7d9d97
Sujal7ss <sujalchahande3@gmail.com>
2025-06-30 20:22:58 +0530

    fixs

--- backend/models/order.js
+++ backend/models/order.js
@@ -46,6 +46,4 @@ const orderSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-
-const Order = mongoose.model("Order", orderSchema);
-export default Order
+export default mongoose.model("Order", orderSchema);
